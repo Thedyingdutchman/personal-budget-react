c86bfa7d3cfdc717c67efba2adab9983bb1ca2b4
Thedyingdutchman <71300765+Thedyingdutchman@users.noreply.github.com>
2020-10-15 21:43:02 -0400

    Followed the video

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <div className="bottom">
+        <div className="center">
+            All rights reserved &copy; Dyllon Thomas
+        </div>
+    </div>
+  );
+}
+
+export default Footer;
